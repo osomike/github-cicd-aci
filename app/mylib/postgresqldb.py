236419cae747a4909faf20fdecb61742731f2b1c
Oscar Mike <mike.claure@gmail.com>
2023-04-01 15:26:26 +0200

    refactor postgresqldb module

--- app/mylib/postgresqldb.py
+++ app/mylib/postgresqldb.py
@@ -1,6 +1,9 @@
-'''Connector for postgreSQL database'''
+"""Connector for postgreSQL database"""
 import os
+import sys
+import pandas as pd
 import sqlalchemy
+from app.mylib.mylogger import logger
 
 
 def get_connector():
@@ -16,4 +19,44 @@ def get_connector():
     # set up database connection
     engine = sqlalchemy.create_engine(f'postgresql://{user}:{password}@{host}/{database}')
 
-    return engine
+    # connector
+    connector = engine.connect()
+    return connector
+
+
+def store_weather_info(info, table_name='weather_data', schema='bronze', if_exists='append'):
+    """
+    Method used to store the weather data into the PostgreSQL database
+    :param info: info to be stored
+    :param table_name: table name
+    :param schema: schema
+    :param if_exists: if exists behavior
+    :return: None
+    """
+    # get connector for DB
+    connector = get_connector()
+
+    # Create DataFrane from weather info
+    info_df = pd.DataFrame.from_dict([info])
+
+    try:
+        logger.info('Storing weather info into the Database...')
+
+        info_df.to_sql(table_name, con=connector.engine, schema=schema, if_exists=if_exists, index=False)
+
+        logger.info('Data stored successfully')
+
+    except Exception as an_exception:
+
+        logger.error(f'Something went wrong. Error: {an_exception}')
+
+        connector.close()
+        logger.info('Connector closed')
+
+        logger.info('Exiting now...')
+
+        sys.exit(1)
+
+    # close connector
+    connector.close()
+    logger.info('Connector closed')
